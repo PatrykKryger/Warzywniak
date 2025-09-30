64a219a95a73112eb089c98d57c54b2c0ba4a561
HeinrichKamil <hansolo12344321@gmail.com>
2025-09-30 15:39:12 +0200

    Added Login and Register route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import Oferta from "./pages/Oferta"
 import Kontakt from "./pages/Kontakt"
+import Login from "./pages/Login.jsx"
+import Register from "./pages/Register.jsx";
 
 function App() {
     return (
@@ -11,8 +13,10 @@ function App() {
             <main style={{ padding: "20px" }}>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/oferta" element={<Oferta />} />
-                    <Route path="/kontakt" element={<Kontakt />} />
+                    <Route path="/Oferta" element={<Oferta />} />
+                    <Route path="/Kontakt" element={<Kontakt />} />
+                    <Route path="/Register" element={<Register />} />
+                    <Route path="/Login" element={<Login />} />
                 </Routes>
             </main>
         </div>
